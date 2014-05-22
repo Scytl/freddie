3449c760dbfbab063490c9529882dd8b484d426d
Pau Fracés <pfraces@gmail.com>
2014-05-22 02:58:50 +0200

    read config from file

--- server.js
+++ server.js
@@ -1,54 +1,91 @@
-var config = {
-    proxy: {
-        '/api': 'http://10.0.16.227:8081'
-    }
-};
+// helpers
+
+var readJSON = (function () {
+    var fs = require('fs');
+            
+    return function (file, options) {
+        var content;
+        options = options || {};
+        
+        try {
+            content = fs.readFileSync(file, { encoding: 'utf8' })
+        } catch (e) {
+            if (e.code === 'ENOENT' && options.fallback) return options.fallback;
+            else throw e;
+        }
+            
+        return JSON.parse(content);
+    };
+})();
+
+var each = (function () {
+    return function (obj, callback) {
+        Object.keys(obj).forEach(function (index) {
+            callback(obj[index], index);
+        });
+    };
+})();
+
+// middlewares
+
+var proxy = (function () {
+    var httpProxy = require('http-proxy');
+    
+    return function (target, context) {
+        var proxy = httpProxy.createProxyServer({});
+        
+        proxy.on('error', function (err, req, res) {
+            var msg = err.code + ': ' + target + req.url;
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end(msg);
+            console.log('[PROXY] ' + msg);
+        });
+        
+        proxy.on('proxyRes', function (ev, req, res) {
+            var msg = req.url + ' -> ' + target + req.url;
+            console.log('[PROXY] ' + msg);
+        });
+        
+        return function (req, res) {
+            if (context) { req.url = context + req.url; }
+            proxy.web(req, res, { target: target });
+        };
+    };
+})();
+
+var mocks = (function () {
+    return function (req, res, next) {
+        req.method = 'GET';
+        next();
+    };
+})();
 
 var connect = require('connect'),
-    proxy = require('http-proxy').createProxyServer({}),
     http = require('http'),
+    path = require('path'),
     getPort = require('./getport');
- 
-var dir = process.argv[2] || process.cwd();
 
-var rewriteToGet = function (req, res, next) {
-    req.method = 'GET';
-    next();
-};
-
-var app = connect();
-
-// proxy the requests as defined in configuration
-Object.keys(config.proxy).forEach(function (context) {
-    var host = config.proxy[context];
-
-    proxy.on('error', function (err, req, res) {
-        var msg = err.code + ': ' + host + req.url;
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(msg);
-        console.log('[PROXY] ' + msg);
-    });
-
-    proxy.on('proxyRes', function (ev, req, res) {
-        var msg = req.url + ' -> ' + host + req.url;
-        console.log('[PROXY] ' + msg);
-    });
-
-    app.use(context, function (req, res) {
-        req.url = context + req.url;
-	proxy.web(req, res, { target: host });
-    });
-});
-
-// if not intercepted by proxy, rewrite all http methods to get
-app.use(rewriteToGet)
-
-// finally return static content
-app.use(connect.static(dir));
+var prj = readJSON(path.join(__dirname, 'package.json')),
+    cwd = process.cwd();
  
 getPort(function (err, port) {
-  if (err) throw err;
+    if (err) { throw err; }
+  
+    var config = readJSON(path.join(cwd, '.' + prj.name + '.json'), { fallback: {} }),
+        root = process.argv[2] || config.root || cwd,
+        app = connect();
+    
+    // proxy the requests as defined in configuration
+    if (config.proxies) {
+        each(config.proxies, function (target, context) {
+            app.use(context, proxy(target, context));
+        });
+    }
+    
+    // if not intercepted by proxy, serve static content
+    app.use(mocks);
+    app.use(connect.static(root));
 
-  http.createServer(app).listen(port);
-  console.log('listening to port ' + port);
+    http.createServer(app).listen(port);
+    console.log('listening to port ' + port);
 });
