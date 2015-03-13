f1ef00a65d183996318ddc6df5c12a143c2be0c6
Pau Fracés <pau.fraces@scytl.com>
2015-03-13 19:45:38 +0100

    [bugfix] accept lowercase cookie attributes

--- src/utils/cookieRewrite.js
+++ src/utils/cookieRewrite.js
@@ -32,6 +32,15 @@ var cookieRewrite = function (cookie, fn) {
     value: value
   };
 
+  // accept lowercase attributes
+
+  attrs['Max-Age']  = attrs['Max-Age']  || attrs['max-age'];
+  attrs['Expires']  = attrs['Expires']  || attrs['expires'];
+  attrs['Path']     = attrs['Path']     || attrs['path'];
+  attrs['Domain']   = attrs['Domain']   || attrs['domain'];
+  attrs['Secure']   = attrs['Secure']   || attrs['secure'];
+  attrs['HttpOnly'] = attrs['HttpOnly'] || attrs['httponly'];
+
   /**
    * Add cookie defined attributes only
    * ----------------------------------
@@ -45,11 +54,11 @@ var cookieRewrite = function (cookie, fn) {
    * For consistency, the approach is extended to all the attributes
    */
 
-  if (attrs['Max-Age']) { attrsMap.maxage = attrs['Max-Age']; }
-  if (attrs['Expires']) { attrsMap.expires = new Date(attrs['Expires']); }
-  if (attrs['Path']) { attrsMap.path = attrs['Path']; }
-  if (attrs['Domain']) { attrsMap.domain = attrs['Domain']; }
-  if (attrs['Secure']) { attrsMap.secure = attrs['Secure']; }
+  if (attrs['Max-Age'])  { attrsMap.maxage   = attrs['Max-Age']; }
+  if (attrs['Expires'])  { attrsMap.expires  = new Date(attrs['Expires']); }
+  if (attrs['Path'])     { attrsMap.path     = attrs['Path']; }
+  if (attrs['Domain'])   { attrsMap.domain   = attrs['Domain']; }
+  if (attrs['Secure'])   { attrsMap.secure   = attrs['Secure']; }
   if (attrs['HttpOnly']) { attrsMap.httpOnly = attrs['HttpOnly']; }
 
   var parsedCookie = fn(attrsMap);
