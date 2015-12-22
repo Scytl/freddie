ca926b78d8acaef4869b00b11bf715922d9cd1ff
Pau Fracés <pau.fraces@scytl.com>
2015-12-22 20:29:40 +0100

    [bugfix] fixed notfound proxy parameters

--- src/middleware/notfound.js
+++ src/middleware/notfound.js
@@ -3,12 +3,15 @@
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function (defaultFile) {
+module.exports = function (config) {
+  var status = config.status || 404;
+  var filePath = config.path;
+
   return function (req, res) {
-    res.writeHead(200, {
+    res.writeHead(status, {
       'Content-Type': 'text/html'
     });
 
-    fs.createReadStream(path.resolve(defaultFile)).pipe(res);
+    fs.createReadStream(path.resolve(filePath)).pipe(res);
   };
 };
