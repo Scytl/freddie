3ac0621d33898edd2f5484a2f975250bcb1ea1e7
Alberto Maldonado <alberto.maldonado@scytl.com>
2016-03-30 10:05:30 +0200

    Update logger with more semantic approach

--- src/utils/log.js
+++ src/utils/log.js
@@ -3,24 +3,44 @@
 var chalk = require('chalk');
 var moment = require('moment');
 
-var red = chalk.red;
-var green = chalk.green;
-var blue = chalk.blue;
-var yellow = chalk.yellow;
-var hour = chalk.grey.bold;
+var success = chalk.green;
+var error = chalk.red;
+var time = chalk.grey.bold;
+var url = chalk.yellow;
+var info = chalk.white;
 
-var log = function (serverName, middlewareName, method, from, to, status) {
-  if (arguments.length === 3) {
-    console.log(red(a), hour(moment().format('H:mm:ss')), red(method));
-    return;
+var log = function (serverName, middlewareName, msg) {
+  /*msg.method = method;
+  msg.from = from;
+  msg.to = to;
+  msg.status = statusCode;*/
+  var sections = [];
+  var status = statusColor(msg.statusCode);
+  if (msg.error) {
+    sections = [
+      time(moment().format('H:mm:ss')),
+      info(serverName),
+      info(middlewareName),
+      status(msg.error),
+      url(msg.to),
+    ];
+  } else {
+    sections = [
+      time(moment().format('H:mm:ss')),
+      info(serverName),
+      info(middlewareName),
+      status(msg.method),
+      url(msg.from),
+      info('->'),
+      url(msg.to),
+      status(msg.statusCode)
+    ];
   }
-  var color = green;
-  if (status >= 400) {
-    color = red;
-  }
-
-  console.log('%s %s' + hour(' %s ') + color('[%s] ') + yellow('%s -> %s ') + color('[%d]'),
-              serverName, middlewareName, moment().format('H:mm:ss'), method, from, to, status);
+  console.log(sections.join(' '));
 };
+
+var statusColor = function (statusCode) {
+  return statusCode < 400 ? success : error;
+}
 
 module.exports = log;
